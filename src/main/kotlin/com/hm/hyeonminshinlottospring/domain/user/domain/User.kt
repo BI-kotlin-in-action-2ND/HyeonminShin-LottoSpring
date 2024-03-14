91033f80b4479e32039c424fff115fa508adbf52
Karatus <shm1193@gmail.com>
2024-03-14 21:14:01 +0900

    feat: User 도메인 추가

---
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/user/domain/User.kt
@@ -0,0 +1,42 @@
+package com.hm.hyeonminshinlottospring.domain.user.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "users")
+class User(
+    // 현재는 유저가 이름으로 구분되어지게끔 만들었습니다. 이후 로그인 ID로 구분하면 좋을것 같습니다.
+    @Column(nullable = false, updatable = false, unique = true)
+    val userName: String,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    val userRole: UserRole = UserRole.ROLE_USER,
+    money: Int = 0,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "users_id")
+    val id: Long? = null,
+) {
+    @Column(nullable = false)
+    var money: Int = money
+        protected set
+
+    fun addMoney(addMoney: Int): Int {
+        require(addMoney > 0) { "0원 이상의 금액을 입력해주세요." }
+        money += addMoney
+        return money
+    }
+
+    fun withdrawMoney(withdrawMoney: Int): Int {
+        require(withdrawMoney > 0) { "출금할 값을 양수로 입력해주세요." }
+        money -= withdrawMoney
+        return money
+    }
+}
