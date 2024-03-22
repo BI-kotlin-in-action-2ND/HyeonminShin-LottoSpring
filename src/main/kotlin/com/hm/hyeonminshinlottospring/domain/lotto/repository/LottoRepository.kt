4e6937e8741bfd4493d2de0c81974ebe46a441b1
Karatus <shm1193@gmail.com>
2024-03-22 16:58:24 +0900

    feat: 불필요한 네이밍 변경 및 반환형 변경에 따른 함수명 변경

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/repository/LottoRepository.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/repository/LottoRepository.kt
@@ -1,34 +1,35 @@
 package com.hm.hyeonminshinlottospring.domain.lotto.repository
 
 import com.hm.hyeonminshinlottospring.domain.lotto.domain.Lotto
-import com.hm.hyeonminshinlottospring.domain.user.domain.User
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
-fun LottoRepository.getByLottoId(lottoId: Long): Lotto =
-    findByIdOrNull(
-        lottoId,
-    ) ?: throw NoSuchElementException("$lottoId: 해당 로또가 존재하지 않습니다.")
-
-// TODO: userId로 바꾸기
-fun LottoRepository.getByUser(user: User): List<Lotto> = findAllByUser(user)
-
-fun LottoRepository.getByRound(round: Int): List<Lotto> = findAllByRound(round)
-
-fun LottoRepository.getByUserAndRound(
-    user: User,
-    round: Int,
-): List<Lotto> = findAllByUserAndRound(user, round)
+fun LottoRepository.findByLottoId(lottoId: Long): Lotto =
+    findByIdOrNull(lottoId) ?: throw NoSuchElementException("$lottoId: 해당 로또가 존재하지 않습니다.")
 
 @Repository
 interface LottoRepository : JpaRepository<Lotto, Long> {
-    fun findAllByUser(user: User): List<Lotto>
+    fun findSliceByUserId(
+        userId: Long,
+        pageable: Pageable,
+    ): Slice<Lotto>
 
-    fun findAllByRound(round: Int): List<Lotto>
+    fun findSliceByRound(
+        round: Int,
+        pageable: Pageable,
+    ): Slice<Lotto>
 
-    fun findAllByUserAndRound(
-        user: User,
+    fun findSliceByUserIdAndRound(
+        userId: Long,
+        round: Int,
+        pageable: Pageable,
+    ): Slice<Lotto>
+
+    fun findListByUserIdAndRound(
+        userId: Long,
         round: Int,
     ): List<Lotto>
 }
