a2d28ecc78c9084cb149bf02d3108b5aabfec053
Karatus <shm1193@gmail.com>
2024-03-22 17:03:46 +0900

    refactor: 불필요한 레퍼 함수 제거

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/user/repository/UserRepository.kt
@@ -5,16 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
-fun UserRepository.existsByUserId(userId: Long): Boolean = existsById(userId)
-
-fun UserRepository.getByUserId(userId: Long): User = findByIdOrNull(userId) ?: throw NoSuchElementException("$userId: 사용자가 존재하지 않습니다.")
-
-fun UserRepository.getByUserName(userName: String): User =
-    findUserByUserName(userName) ?: throw NoSuchElementException("$userName: 사용자가 존재하지 않습니다.")
+fun UserRepository.findByUserId(userId: Long): User = findByIdOrNull(userId) ?: throw NoSuchElementException("$userId: 사용자가 존재하지 않습니다.")
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun existsByUserName(userName: String): Boolean
 
-    fun findUserByUserName(userName: String): User?
+    fun findByUserName(userName: String): User?
 }
