b4422b60d0e8920d1c184d163a1544b4ffd49d67
Karatus <shm1193@gmail.com>
2024-03-22 16:54:02 +0900

    feat: 유저 인덱스 추가 및 로또 번호의 value class 반영

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/domain/Lotto.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/domain/Lotto.kt
@@ -1,66 +1,63 @@
 package com.hm.hyeonminshinlottospring.domain.lotto.domain
 
+import com.hm.hyeonminshinlottospring.domain.lotto.domain.converter.LottoNumberAttributeConverter
 import com.hm.hyeonminshinlottospring.domain.user.domain.User
 import com.hm.hyeonminshinlottospring.global.support.doamin.BaseTimeEntity
-import jakarta.persistence.CollectionTable
 import jakarta.persistence.Column
-import jakarta.persistence.ElementCollection
+import jakarta.persistence.Convert
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Index
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
-import java.util.SortedSet
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
 
 // TODO: 최적화 솔루션) numbers를 bitset으로 다루기!!
 
+@Table(
+    indexes = [
+        Index(name = "user_id_index", columnList = "user_id"),
+    ],
+)
+@SequenceGenerator(
+    name = "LOTTO_SEQ_GENERATOR",
+    sequenceName = "LOTTO_SEQ",
+    initialValue = 1,
+    allocationSize = 1,
+)
 @Entity
 class Lotto(
     @Column(nullable = false, updatable = false)
     val round: Int,
-    numbers: SortedSet<LottoNumber>,
+    @Convert(converter = LottoNumberAttributeConverter::class)
+    val numbers: LottoNumber,
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "users_id", updatable = false)
+    @JoinColumn(name = "user_id", updatable = false)
     val user: User,
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "lotto_id")
-    val id: Long? = null,
+    @Column(columnDefinition = "NUMERIC(19, 0)")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOTTO_SEQ_GENERATOR")
+    val id: Long = 0L,
 ) : BaseTimeEntity() {
     // TODO: bonus 번호 프로퍼티를 추가하기
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "lotto_numbers", joinColumns = [JoinColumn(name = "lotto_id")])
-    var numbers: SortedSet<LottoNumber> = numbers
-        protected set
-
-    init {
-        // 중복되지 않은 번호 개수 체크
-        require(numbers.size == NUM_OF_LOTTO_NUMBERS) { "중복을 제외한 ${NUM_OF_LOTTO_NUMBERS}개의 번호가 필요합니다." }
-    }
-
     constructor(
         round: Int,
         numbers: Collection<Int>,
         user: User,
-        id: Long? = null,
-        @Suppress("UNUSED_PARAMETER") dummyImplicit: Any? = null,
+        id: Long = 0L,
     ) : this(
         round = round,
         user = user,
-        numbers = numbers.map { LottoNumber(it) }.toSortedSet(),
+        numbers = LottoNumber(numbers),
         id = id,
     )
 
     fun match(other: Lotto): List<Int> {
         val intersect = this.numbers.intersect(other.numbers)
-        return intersect.map { it.number }.toList()
-    }
-
-    override fun toString() = numbers.joinToString(" ")
-
-    companion object {
-        const val NUM_OF_LOTTO_NUMBERS = 6
+        return intersect.toList()
     }
 }
