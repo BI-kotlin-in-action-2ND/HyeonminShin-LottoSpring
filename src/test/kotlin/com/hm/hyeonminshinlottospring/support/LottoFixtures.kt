5e46e800767d7ab814e1609867e139709b44db1b
Karatus <shm1193@gmail.com>
2024-03-22 17:16:32 +0900

    test: 테스트용 fixtures 수정 및 추가

--- src/test/kotlin/com/hm/hyeonminshinlottospring/support/LottoFixtures.kt
+++ src/test/kotlin/com/hm/hyeonminshinlottospring/support/LottoFixtures.kt
@@ -4,117 +4,121 @@ import com.hm.hyeonminshinlottospring.domain.lotto.domain.GenerateMode
 import com.hm.hyeonminshinlottospring.domain.lotto.domain.Lotto
 import com.hm.hyeonminshinlottospring.domain.lotto.domain.LottoNumber
 import com.hm.hyeonminshinlottospring.domain.lotto.dto.LottoCreateRequest
+import com.hm.hyeonminshinlottospring.domain.lotto.dto.LottoCreateResponse
+import com.hm.hyeonminshinlottospring.domain.lotto.dto.SliceLottoNumberResponse
 import com.hm.hyeonminshinlottospring.domain.user.domain.User
-import java.util.SortedSet
 
 const val TEST_LOTTO_ID = 12L
 const val TEST_OTHER_LOTTO_ID = 13L
 const val TEST_ANOTHER_LOTTO_ID = 14L
-const val TEST_NOT_EXIST_LOTTO_ID = 15L
+const val TEST_NOT_EXIST_LOTTO_ID = 1000L
 const val TEST_INVALID_LOTTO_ID = -1L
 const val TEST_ROUND = 1001
 const val TEST_OTHER_ROUND = 1002
 const val TEST_ANOTHER_ROUND = 1003
 const val TEST_NOT_EXIST_ROUND = 4
 const val TEST_INVALID_ROUND = -1
-const val TEST_GENERATE_COUNT = 5
+const val TEST_GENERATE_COUNT_2 = 2
+const val TEST_GENERATE_COUNT_10 = 10
 val TEST_NUMBER =
     (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
         .shuffled()
-        .take(Lotto.NUM_OF_LOTTO_NUMBERS)
-        .toSortedSet()
+        .take(LottoNumber.NUM_OF_LOTTO_NUMBERS)
 val TEST_NUMBER_2 =
     (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
         .shuffled()
-        .take(Lotto.NUM_OF_LOTTO_NUMBERS)
-        .toSortedSet()
+        .take(LottoNumber.NUM_OF_LOTTO_NUMBERS)
 val TEST_OTHER_NUMBER =
     (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
         .shuffled()
-        .take(Lotto.NUM_OF_LOTTO_NUMBERS)
-        .toSortedSet()
+        .take(LottoNumber.NUM_OF_LOTTO_NUMBERS)
 val TEST_NUMBER_NODUP_BUT_OVERSIZE =
     (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
         .shuffled()
-        .take(Lotto.NUM_OF_LOTTO_NUMBERS + 1)
-        .toSortedSet()
+        .take(LottoNumber.NUM_OF_LOTTO_NUMBERS + 1)
 val TEST_NUMBER_NODUP_BUT_UNDERSIZE =
     (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
         .shuffled()
-        .take(Lotto.NUM_OF_LOTTO_NUMBERS - 1)
-        .toSortedSet()
+        .take(LottoNumber.NUM_OF_LOTTO_NUMBERS - 1)
 val TEST_NUMBER_NORMALSIZE_BUT_DUP =
     (LottoNumber.LOTTO_MIN_NUMBER..<LottoNumber.LOTTO_MAX_NUMBER)
-        .take(Lotto.NUM_OF_LOTTO_NUMBERS - 2) + LottoNumber.LOTTO_MAX_NUMBER + LottoNumber.LOTTO_MAX_NUMBER
+        .take(LottoNumber.NUM_OF_LOTTO_NUMBERS - 2) + LottoNumber.LOTTO_MAX_NUMBER + LottoNumber.LOTTO_MAX_NUMBER
 
 fun createLotto(
     user: User,
-    numbers: SortedSet<LottoNumber> = TEST_NUMBER,
+    numbers: Collection<Int> = TEST_NUMBER,
 ) = Lotto(
     round = TEST_ROUND,
     numbers = numbers,
     user = user,
-//        id = TEST_LOTTO_ID,
 )
 
 fun createOtherLotto(
     user: User,
-    numbers: SortedSet<LottoNumber> = TEST_OTHER_NUMBER,
+    numbers: Collection<Int> = TEST_OTHER_NUMBER,
 ) = Lotto(
     round = TEST_ROUND,
     numbers = numbers,
     user = user,
-//    id = TEST_OTHER_LOTTO_ID,
 )
 
-@JvmName("lottoWithInt")
 fun createCustomLotto(
     round: Int,
     numbers: Collection<Int>,
     user: User,
-    id: Long? = null,
 ) = Lotto(
     round = round,
     numbers = numbers,
     user = user,
-    id = id,
 )
 
-@JvmName("lottoWithLottoNumber")
-fun createCustomLotto(
-    round: Int,
-    numbers: Collection<LottoNumber>,
-    user: User,
-    id: Long? = null,
-) = Lotto(
-    round = round,
-    numbers = numbers.toSortedSet(),
-    user = user,
-    id = id,
-)
-
-fun createLottoCreateRequest(
-    round: Int = TEST_ROUND,
-    mode: GenerateMode = GenerateMode.RANDOM,
-    generateCount: Int = TEST_GENERATE_COUNT,
-    numbers: List<List<Int>>?,
-) = LottoCreateRequest(
-    round = round,
-    mode = mode,
-    generateCount = generateCount,
-    numbers = numbers,
-)
+fun createLottoNumber(list: Collection<Int> = TEST_NUMBER) = LottoNumber(list)
 
 fun createLottoNumbers(count: Int = 1): List<List<Int>> {
     val mutableList = mutableListOf<List<Int>>()
     repeat(count) {
         mutableList.add(
-            (LottoNumber.RAW_LOTTO_MIN_NUMBER..LottoNumber.RAW_LOTTO_MAX_NUMBER)
+            (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
                 .shuffled()
-                .take(Lotto.NUM_OF_LOTTO_NUMBERS),
+                .take(LottoNumber.NUM_OF_LOTTO_NUMBERS),
         )
     }
     return mutableList.toList()
 }
 
-fun createAllLottoWithUser(user: User) = listOf(createLotto(user), createLotto(user, TEST_NUMBER_2))
+fun createAllLotto(user: User) = listOf(createLotto(user), createLotto(user, TEST_NUMBER_2))
+fun createLottoCreateRequest(
+    userId: Long = TEST_USER_ID,
+    insertedMoney: Int = TEST_MONEY_10,
+    numbers: List<List<Int>>? = null,
+    mode: GenerateMode = GenerateMode.RANDOM,
+) = LottoCreateRequest(
+    userId = userId,
+    mode = mode,
+    insertedMoney = insertedMoney,
+    numbers = numbers,
+)
+
+fun createLottoCreateResponse(
+    userId: Long = TEST_USER_ID,
+    round: Int = TEST_ROUND,
+    createdLottoCount: Int = TEST_GENERATE_COUNT_10,
+) = LottoCreateResponse(
+    userId = userId,
+    round = round,
+    createdLottoCount = createdLottoCount,
+)
+
+fun createSliceLottoNumberResponse(
+    userId: Long? = TEST_USER_ID,
+    round: Int? = TEST_ROUND,
+    hasNext: Boolean = true,
+    numberOfElements: Int = TEST_GENERATE_COUNT_2,
+    numberList: List<String>? = null,
+) = SliceLottoNumberResponse(
+    userId = userId,
+    round = round,
+    hasNext = hasNext,
+    numberOfElements = numberOfElements,
+    numberList = createLottoNumbers(numberOfElements).map { it.joinToString() },
+)
--- src/test/kotlin/com/hm/hyeonminshinlottospring/support/UserFixtures.kt
+++ src/test/kotlin/com/hm/hyeonminshinlottospring/support/UserFixtures.kt
@@ -3,12 +3,13 @@ package com.hm.hyeonminshinlottospring.support
 import com.hm.hyeonminshinlottospring.domain.user.domain.User
 import com.hm.hyeonminshinlottospring.domain.user.domain.UserRole
 import com.hm.hyeonminshinlottospring.domain.user.dto.UserCreateRequest
+import com.hm.hyeonminshinlottospring.domain.user.dto.UserResponse
 
-const val TEST_USER_ID = 11L
-const val TEST_OTHER_USER_ID = 12L
-const val TEST_ANOTHER_USER_ID = 13L
-const val TEST_ADMIN_USER_ID = 14L
-const val TEST_NOT_EXIST_USER_ID = 15L
+const val TEST_USER_ID = 1001L
+const val TEST_OTHER_USER_ID = 1002L
+const val TEST_ANOTHER_USER_ID = 1003L
+const val TEST_ADMIN_USER_ID = 1004L
+const val TEST_NOT_EXIST_USER_ID = 1005L
 const val TEST_INVALID_USER_ID = -1L
 const val TEST_USERNAME = "testUsername"
 const val TEST_OTHER_USERNAME = "testOtherUsername"
@@ -20,36 +21,41 @@ const val TEST_INVALID_MONEY = -1
 val TEST_USER_ROLE_USER = UserRole.ROLE_USER
 val TEST_USER_ROLE_ADMIN = UserRole.ROLE_ADMIN
 
-fun createUser() =
-    User(
-        userName = TEST_USERNAME,
-        userRole = TEST_USER_ROLE_USER,
-        money = TEST_MONEY_10,
-//        id = TEST_USER_ID,
-    )
-
-fun createOtherUser() =
-    User(
-        userName = TEST_OTHER_USERNAME,
-        userRole = TEST_USER_ROLE_USER,
-        money = TEST_MONEY_10,
-//        id = TEST_OTHER_USER_ID,
-    )
-
-fun createAdmin() =
-    User(
-        userName = TEST_ADMIN_USERNAME,
-        userRole = TEST_USER_ROLE_ADMIN,
-        money = TEST_MONEY_ZERO,
-//        id = TEST_ADMIN_USER_ID,
-    )
-
-fun createUserCreateRequest(
-    userName: String,
-    money: Int,
-    userRole: UserRole,
-) = UserCreateRequest(
+fun createUser(
+    userName: String = TEST_USERNAME,
+    userRole: UserRole = TEST_USER_ROLE_USER,
+    money: Int = TEST_MONEY_10,
+) = User(
     userName = userName,
-    money = money,
     userRole = userRole,
+    money = money,
 )
+
+fun createOtherUser(
+    userName: String = TEST_OTHER_USERNAME,
+    userRole: UserRole = TEST_USER_ROLE_USER,
+    money: Int = TEST_MONEY_10,
+) = User(
+    userName = userName,
+    userRole = userRole,
+    money = money,
+)
+
+fun createAdmin(
+    userName: String = TEST_ADMIN_USERNAME,
+    userRole: UserRole = TEST_USER_ROLE_ADMIN,
+    money: Int = TEST_MONEY_10,
+) = User(
+    userName = userName,
+    userRole = userRole,
+    money = money,
+)
+
+fun createUserCreateRequest(user: User) =
+    UserCreateRequest(
+        userName = user.userName,
+        money = user.money,
+        userRole = user.userRole,
+    )
+
+fun createUserResponse(user: User) = UserResponse.from(user)
