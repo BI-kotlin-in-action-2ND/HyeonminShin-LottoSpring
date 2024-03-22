523dfbdf94901a4bcc61075943f3e3a5dd8a042d
Karatus <shm1193@gmail.com>
2024-03-22 17:07:40 +0900

    feat: lotto 인덱스, id 시퀀스 추가

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/domain/WinningLotto.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/domain/WinningLotto.kt
@@ -6,9 +6,23 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Index
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
 
+@Table(
+    indexes = [
+        Index(name = "lotto_id_index", columnList = "lotto_id"),
+    ],
+)
+@SequenceGenerator(
+    name = "WINNING_LOTTO_SEQ_GENERATOR",
+    sequenceName = "WINNING_LOTTO_SEQ",
+    initialValue = 1,
+    allocationSize = 1,
+)
 @Entity
 class WinningLotto(
     val round: Int,
@@ -16,7 +30,7 @@ class WinningLotto(
     @JoinColumn(name = "lotto_id", updatable = false, nullable = false)
     val lotto: Lotto,
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "winning_lotto_id")
-    val id: Long? = null,
+    @Column(columnDefinition = "NUMERIC(19, 0)")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "WINNING_LOTTO_SEQ_GENERATOR")
+    val id: Long = 0L,
 )
