9ff4f9490b7978f195095aa3978c9580ebd1e363
Karatus <shm1193@gmail.com>
2024-03-22 17:06:03 +0900

    refactor: URI 및 리턴 타입 조정

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/cotroller/WinningLottoController.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/cotroller/WinningLottoController.kt
@@ -1,14 +1,13 @@
 package com.hm.hyeonminshinlottospring.domain.winninglotto.cotroller
 
-import com.hm.hyeonminshinlottospring.domain.winninglotto.dto.WinningLottoMatchResponse
+import com.hm.hyeonminshinlottospring.domain.winninglotto.dto.TotalMatchResponse
 import com.hm.hyeonminshinlottospring.domain.winninglotto.dto.WinningLottoRoundResponse
 import com.hm.hyeonminshinlottospring.domain.winninglotto.service.WinningLottoService
-import com.hm.hyeonminshinlottospring.global.support.dto.SliceResponse
 import jakarta.validation.constraints.Positive
 import org.springframework.http.ResponseEntity
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,42 +18,29 @@ import org.springframework.web.bind.annotation.RestController
 class WinningLottoController(
     private val winningLottoService: WinningLottoService,
 ) {
-    @Scheduled(fixedRate = PER_ROUND_TIME)
-    fun createWinningLotto() {
-        winningLottoService.createWinningLotto()
-    }
-
-    @GetMapping
+    @GetMapping("/{round}")
     fun getWinningLottoByRound(
         @Positive(message = "유저 ID는 0보다 커야 합니다.")
         @RequestParam("userId")
         userId: Long,
         @Positive(message = "라운드는 양수여야 합니다.")
-        @RequestParam("round")
+        @PathVariable("round")
         round: Int,
     ): ResponseEntity<WinningLottoRoundResponse> {
         val response = winningLottoService.getWinningLottoByRound(userId, round)
         return ResponseEntity.ok(response)
     }
 
-    @GetMapping("/match")
+    @GetMapping("/{round}/match")
     fun matchUserLottoByRound(
         @Positive(message = "유저 ID는 0보다 커야 합니다.")
         @RequestParam("userId")
         userId: Long,
         @Positive(message = "라운드는 양수여야 합니다.")
-        @RequestParam("round")
+        @PathVariable("round")
         round: Int,
-    ): ResponseEntity<SliceResponse<WinningLottoMatchResponse>> {
+    ): ResponseEntity<TotalMatchResponse> {
         val response = winningLottoService.matchUserLottoByRound(userId, round)
         return ResponseEntity.ok(response)
-    }
-
-    companion object {
-        private const val SECOND = 1000L
-        private const val MIN = 60L * SECOND
-        private const val HOUR = 60L * MIN
-
-        const val PER_ROUND_TIME = 10L * MIN
     }
 }
