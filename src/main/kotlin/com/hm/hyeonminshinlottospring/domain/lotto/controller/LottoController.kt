56b964df8b296e0ebb507cdf059b556aae311656
Karatus <shm1193@gmail.com>
2024-03-22 17:00:50 +0900

    refactor: 서비스 리턴 타입 반영

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/controller/LottoController.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/controller/LottoController.kt
@@ -1,11 +1,13 @@
 package com.hm.hyeonminshinlottospring.domain.lotto.controller
 
 import com.hm.hyeonminshinlottospring.domain.lotto.dto.LottoCreateRequest
-import com.hm.hyeonminshinlottospring.domain.lotto.dto.LottoResponse
+import com.hm.hyeonminshinlottospring.domain.lotto.dto.LottoCreateResponse
+import com.hm.hyeonminshinlottospring.domain.lotto.dto.SliceLottoNumberResponse
 import com.hm.hyeonminshinlottospring.domain.lotto.service.LottoService
-import com.hm.hyeonminshinlottospring.global.support.dto.SliceResponse
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Positive
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
@@ -25,15 +27,13 @@ class LottoController(
 ) {
     @PostMapping
     fun createLottos(
-        @Positive(message = "유저 ID는 0보다 커야 합니다.")
-        @RequestParam("userId")
-        userId: Long,
         @Valid
         @RequestBody
         lottoCreateRequest: LottoCreateRequest,
-    ): ResponseEntity<Void> {
-        val response = lottoService.createLottos(userId, lottoCreateRequest)
-        return ResponseEntity.created(URI.create("/api/v1/lotto/$userId?round=${response.round}&size=${response.size}")).build()
+    ): ResponseEntity<LottoCreateResponse> {
+        val response = lottoService.createLottos(lottoCreateRequest)
+        return ResponseEntity.created(URI.create("/api/v1/lotto/${response.userId}"))
+            .body(response)
     }
 
     @GetMapping("/{userId}")
@@ -44,11 +44,10 @@ class LottoController(
         @Positive(message = "라운드는 양수여야 합니다.")
         @RequestParam("round")
         round: Int,
-        @Positive(message = "조회할 개수는 양수여야 합니다.")
-        @RequestParam("size", defaultValue = "1", required = false)
-        size: Int?,
-    ): ResponseEntity<SliceResponse<LottoResponse>> {
-        val response = lottoService.getLottosByUserAndRound(userId, round, size)
+        @PageableDefault(page = 0, size = 10)
+        pageable: Pageable,
+    ): ResponseEntity<SliceLottoNumberResponse> {
+        val response = lottoService.getLottosByUserAndRound(userId, round, pageable)
         return ResponseEntity.ok(response)
     }
 }
