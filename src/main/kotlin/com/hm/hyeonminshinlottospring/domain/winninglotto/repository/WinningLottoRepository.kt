75284d7c20ccb98e7cec6c20ec3dbaa52d5808fc
Karatus <shm1193@gmail.com>
2024-03-22 17:08:48 +0900

    refactor: 불필요한 레퍼 함수 제거

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/repository/WinningLottoRepository.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/repository/WinningLottoRepository.kt
@@ -1,14 +1,10 @@
 package com.hm.hyeonminshinlottospring.domain.winninglotto.repository
 
-import com.hm.hyeonminshinlottospring.domain.lotto.domain.Lotto
 import com.hm.hyeonminshinlottospring.domain.winninglotto.domain.WinningLotto
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-fun WinningLottoRepository.getByRound(round: Int): Lotto =
-    findByRound(round)?.lotto ?: throw NoSuchElementException("$round 라운드에 존재하는 로또 번호가 없습니다.")
-
 @Repository
 interface WinningLottoRepository : JpaRepository<WinningLotto, Long> {
-    fun findByRound(round: Int): WinningLotto?
+    fun findByRound(round: Int): WinningLotto? // ?: throw NoSuchElementException("$round 라운드에 해당하는 당첨 번호가 존재하지 않습니다.")
 }
