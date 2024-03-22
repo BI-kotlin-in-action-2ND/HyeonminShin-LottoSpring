25e755f2f594b0a50e932ae4bca4cc5da18f700d
Karatus <shm1193@gmail.com>
2024-03-22 16:55:06 +0900

    feat: data class -> value class
    POJO에 맞도록 설계 및 최적화 완료

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/domain/LottoNumber.kt
@@ -1,52 +1,35 @@
 package com.hm.hyeonminshinlottospring.domain.lotto.domain
 
-import com.hm.hyeonminshinlottospring.global.support.doamin.NoArgsConstructor
-import jakarta.persistence.Embeddable
+import java.util.SortedSet
 
-// TODO: insert 쿼리 너무 많이 나감. 그냥 콤마로 숫자 구분하는 문자열 파서 + 검증기로 만들자.
-@Embeddable
-@NoArgsConstructor
-data class LottoNumber(val number: Int) : Comparable<LottoNumber> {
+@JvmInline
+value class LottoNumber private constructor(private val number: SortedSet<Int>) {
     init {
-        require(number in VALID_RANGE) { NUMBER_RANGE_ERROR_MESSAGE }
+        require(number.size == NUM_OF_LOTTO_NUMBERS) { "중복을 제외한 ${NUM_OF_LOTTO_NUMBERS}개의 번호가 필요합니다." }
+        require(number.all { it in VALID_RANGE }) { NUMBER_RANGE_ERROR_MESSAGE }
     }
 
-    override fun compareTo(other: LottoNumber): Int {
-        return number.compareTo(other.number)
-    }
+    constructor(number: Collection<Int>) : this(number.toSortedSet())
 
-    override fun toString(): String {
-        return "%2d".format(number)
-    }
+    fun joinToString() = this.number.joinToString(DELIMITER)
+    fun intersect(other: LottoNumber) = this.number.intersect(other.number)
 
-    operator fun rangeTo(lottoMaxNumber: LottoNumber): List<LottoNumber> {
-        return (number..lottoMaxNumber.number).map { LottoNumber(it) }
-    }
-
-    operator fun rangeUntil(lottoMaxNumber: LottoNumber) = rangeTo(lottoMaxNumber - 1)
-
-    operator fun plus(i: Int): LottoNumber {
-        return LottoNumber(requireNotNull((number + i).takeIf { it in VALID_RANGE }) { NUMBER_RANGE_ERROR_MESSAGE })
-    }
-
-    operator fun minus(i: Int): LottoNumber {
-        return LottoNumber(requireNotNull((number - i).takeIf { it in VALID_RANGE }) { NUMBER_RANGE_ERROR_MESSAGE })
-    }
-
-    infix fun until(lottoNumber: LottoNumber): List<LottoNumber> {
-        return (number until lottoNumber.number).map { LottoNumber(it) }
-    }
+    fun count(other: LottoNumber) = this.number.count { it in other.number }
 
     companion object {
-        const val RAW_LOTTO_MIN_NUMBER = 1
-        const val RAW_LOTTO_MAX_NUMBER = 45
+        const val LOTTO_MIN_NUMBER = 1
+        const val LOTTO_MAX_NUMBER = 45
 
-        private val VALID_RANGE: IntRange = RAW_LOTTO_MIN_NUMBER..RAW_LOTTO_MAX_NUMBER
+        const val NUM_OF_LOTTO_NUMBERS = 6
 
-        val LOTTO_MIN_NUMBER = LottoNumber(RAW_LOTTO_MIN_NUMBER)
-        val LOTTO_MAX_NUMBER = LottoNumber(RAW_LOTTO_MAX_NUMBER)
+        private val VALID_RANGE: IntRange = LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER
 
-        val NUMBER_RANGE_ERROR_MESSAGE =
+        const val DELIMITER = ","
+
+        const val NUMBER_RANGE_ERROR_MESSAGE =
             "로또 번호는 [$LOTTO_MIN_NUMBER ~ $LOTTO_MAX_NUMBER] 범위여야 합니다."
+
+        @JvmStatic
+        fun from(number: Collection<Int>) = LottoNumber(number.toSortedSet())
     }
 }
