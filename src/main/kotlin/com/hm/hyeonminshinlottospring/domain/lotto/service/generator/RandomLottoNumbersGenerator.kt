af49d6ea491b4ac16863cefcfc624d3fb078ce2f
Karatus <shm1193@gmail.com>
2024-03-22 16:59:07 +0900

    feat: LottoNumber 변경에 따른 리턴 타입 변경

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/service/generator/RandomLottoNumbersGenerator.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/service/generator/RandomLottoNumbersGenerator.kt
@@ -1,6 +1,5 @@
 package com.hm.hyeonminshinlottospring.domain.lotto.service.generator
 
-import com.hm.hyeonminshinlottospring.domain.lotto.domain.Lotto
 import com.hm.hyeonminshinlottospring.domain.lotto.domain.LottoNumber
 import org.springframework.stereotype.Component
 
@@ -9,6 +8,5 @@ class RandomLottoNumbersGenerator {
     fun generate() =
         (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
             .shuffled()
-            .take(Lotto.NUM_OF_LOTTO_NUMBERS)
-            .toSortedSet()
+            .take(LottoNumber.NUM_OF_LOTTO_NUMBERS)
 }
