f06a99acbcbae21780a6f229dd6f0762823fbe48
Karatus <shm1193@gmail.com>
2024-04-05 20:13:39 +0900

    refactor: todo 주석 삭제

--- src/test/kotlin/com/hm/hyeonminshinlottospring/support/test/BaseTests.kt
+++ src/test/kotlin/com/hm/hyeonminshinlottospring/support/test/BaseTests.kt
@@ -3,7 +3,6 @@ package com.hm.hyeonminshinlottospring.support.test
 import com.hm.hyeonminshinlottospring.global.config.JpaAuditingConfig
 import com.hm.hyeonminshinlottospring.global.config.SchedulingConfig
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.context.SpringBootTest
@@ -19,13 +18,11 @@ class BaseTests {
     @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
     annotation class TestEnvironment
 
-    // TODO: application-database.yml에서 [schema,data].sql 파일 생성 후 RepositoryTest 사용 가능
     @TestEnvironment
     @Target(AnnotationTarget.CLASS)
-    @Import(JpaAuditingConfig::class)
     @Retention(AnnotationRetention.RUNTIME)
+    @Import(JpaAuditingConfig::class)
     @DataJpaTest(properties = ["spring.jpa.hibernate.ddl-auto=none"])
-    @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
     annotation class RepositoryTest
 
     @TestEnvironment
@@ -36,6 +33,5 @@ class BaseTests {
 
     @TestEnvironment
     @SpringBootTest
-//    @Import(TestMockBeanConfig::class)
     annotation class IntegrationTest
 }
