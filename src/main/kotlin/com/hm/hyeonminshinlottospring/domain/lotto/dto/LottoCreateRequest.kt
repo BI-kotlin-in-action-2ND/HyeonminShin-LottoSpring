c789375491f1b7cfbf5372fc5788a8585a9c3275
Karatus <shm1193@gmail.com>
2024-04-05 18:36:50 +0900

    refactor: 팩토리 메서드 삭제

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/dto/LottoCreateRequest.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/lotto/dto/LottoCreateRequest.kt
@@ -1,10 +1,6 @@
 package com.hm.hyeonminshinlottospring.domain.lotto.dto
 
 import com.hm.hyeonminshinlottospring.domain.lotto.domain.GenerateMode
-import com.hm.hyeonminshinlottospring.domain.lotto.domain.Lotto
-import com.hm.hyeonminshinlottospring.domain.lotto.domain.info.LottoPrice
-import com.hm.hyeonminshinlottospring.domain.lotto.service.generator.RandomLottoNumbersGenerator
-import com.hm.hyeonminshinlottospring.domain.user.domain.User
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 
@@ -18,45 +14,4 @@ data class LottoCreateRequest(
     @field:Positive(message = "투입할 돈은 양수여야 합니다.")
     val insertedMoney: Int,
     val numbers: List<List<Int>>? = null,
-) {
-    fun toEntities(
-        user: User,
-        round: Int,
-        randomLottoNumbersGenerator: RandomLottoNumbersGenerator,
-    ) = createLottosWithMode(user, round, randomLottoNumbersGenerator)
-
-    private fun createLottosWithMode(
-        user: User,
-        round: Int,
-        randomLottoNumbersGenerator: RandomLottoNumbersGenerator,
-    ): List<Lotto> {
-        val result = mutableListOf<Lotto>()
-        val generateCount = this.insertedMoney / LottoPrice.PER_PRICE
-        when (this.mode) {
-            GenerateMode.RANDOM ->
-                repeat(generateCount) {
-                    result.add(
-                        Lotto(
-                            round = round,
-                            user = user,
-                            numbers = randomLottoNumbersGenerator.generate(),
-                        ),
-                    )
-                }
-
-            GenerateMode.MANUAL -> {
-                val numbersIterator = this.numbers?.listIterator() ?: listOf<List<Int>>().listIterator()
-                while (numbersIterator.hasNext()) {
-                    result.add(
-                        Lotto(
-                            round = round,
-                            user = user,
-                            numbers = numbersIterator.next(),
-                        ),
-                    )
-                }
-            }
-        }
-        return result.toList()
-    }
-}
+)
