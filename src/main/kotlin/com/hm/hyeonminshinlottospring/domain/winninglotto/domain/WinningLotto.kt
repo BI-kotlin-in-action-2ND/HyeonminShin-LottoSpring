d14177fdb33823610eac569811f0d753bf1a8ca6
Karatus <shm1193@gmail.com>
2024-04-05 18:38:27 +0900

    feat: pk 생성 전략 롤백

--- src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/domain/WinningLotto.kt
+++ src/main/kotlin/com/hm/hyeonminshinlottospring/domain/winninglotto/domain/WinningLotto.kt
@@ -1,7 +1,6 @@
 package com.hm.hyeonminshinlottospring.domain.winninglotto.domain
 
 import com.hm.hyeonminshinlottospring.domain.lotto.domain.Lotto
-import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -9,19 +8,12 @@ import jakarta.persistence.Id
 import jakarta.persistence.Index
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
-import jakarta.persistence.SequenceGenerator
 import jakarta.persistence.Table
 
 @Table(
     indexes = [
         Index(name = "lotto_id_index", columnList = "lotto_id"),
     ],
-)
-@SequenceGenerator(
-    name = "WINNING_LOTTO_SEQ_GENERATOR",
-    sequenceName = "WINNING_LOTTO_SEQ",
-    initialValue = 1,
-    allocationSize = 1,
 )
 @Entity
 class WinningLotto(
@@ -30,7 +22,6 @@ class WinningLotto(
     @JoinColumn(name = "lotto_id", updatable = false, nullable = false)
     val lotto: Lotto,
     @Id
-    @Column(columnDefinition = "NUMERIC(19, 0)")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "WINNING_LOTTO_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
 )
